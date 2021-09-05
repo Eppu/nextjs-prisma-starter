bc8e7cd2f625c539fadf84e54164b593715835d8
Eetu Eskelinen <edwardtehgreat@gmail.com>
2021-09-05 18:36:49 +0300

    Add publish functionality

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,9 +1,11 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
-import prisma from "../../lib/prisma"
+import React from "react";
+import { GetServerSideProps } from "next";
+import ReactMarkdown from "react-markdown";
+import Layout from "../../components/Layout";
+import Router from "next/router";
+import { PostProps } from "../../components/Post";
+import { useSession } from "next-auth/client";
+import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -12,16 +14,30 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       }
     }
   });
   return {
     props: post,
   }
+};
+
+async function publishPost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/publish/${id}`, {
+    method: 'PUT'
+  });
+  await Router.push('/');
 }
 
 const Post: React.FC<PostProps> = (props) => {
+  const [session, loading] = useSession();
+  if (loading) {
+    return <div>Authenticating...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
+ 
   let title = props.title
   if (!props.published) {
     title = `${title} (Draft)`
@@ -33,6 +49,9 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || "Unknown author"}</p>
         <ReactMarkdown source={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
       </div>
       <style jsx>{`
         .page {
